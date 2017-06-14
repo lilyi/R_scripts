8114dd378d1c43e038015bf7a4c62d347fe82d28
lilyi <lilyli@qnap.com>
2017-06-14 17:21:17 +0800

    update

--- total_sessions_stat_v3.R
+++ total_sessions_stat_v3.R
@@ -1,4 +1,5 @@
-
+library(ggplot2)
+library(scales)
 mdir <- "mkdir C:\\Users\\Lily\\Documents\\GA\\R\\report\\2017\\Monthly\\trend\\"
 fnames <- "May"
 f_list <- unlist(lapply(mdir, paste, fnames, sep=""))
@@ -87,7 +88,7 @@ trend_Q1_M45 <- function(cname){
 }
 # trend_Q1_M45(cname)
 A <- as.data.frame(lapply(clist, trend_Q1_M45))
-mon <- "May"
+mon <- "April"
 trend_mon <- function(cname, mon){
   Q1_data <- read.csv(paste("C:/Users/Lily/Documents/GA/R/report/2017/Monthly/Q1/total_dat/", cname, '.csv', sep=""), header=T)
   Q1_data$day <- weekdays.Date(as.Date(as.character(Q1_data$date), format="%Y%m%d"))
@@ -104,14 +105,14 @@ trend_mon <- function(cname, mon){
   Q1_data$CMA <- c(rep(0,3), CMA, rep(0,3))
   std <- round(sd(CMA),4)
   mean <- round(mean(CMA),2)
-  upper <- round((mean + 1.5*std),2)
-  lower <- round((mean - 1.5*std),2)
+  upper <- round((mean + 2*std),2)
+  lower <- round((mean - 2*std),2)
   
   M_data <- read.csv(paste("C:/Users/Lily/Documents/GA/R/report/2017/Monthly/", mon, "/total_dat/", cname, '.csv', sep=""), header=T)
   M_data$day <- weekdays.Date(as.Date(as.character(M_data$date), format="%Y%m%d"))
   M_data$day <- match(M_data$day[], day_trans[,2])
   M_len <- length(M_data$sessions)-6
-  M_CMA=numeric(len)  
+  M_CMA=numeric(M_len)  
   for(i in 1:M_len)
   { 
     M_CMA[i]=(M_data[i,3]+M_data[i+1,3]+M_data[i+2,3]+M_data[i+3,3]+M_data[i+4,3]+M_data[i+5,3]+M_data[i+6,3])/7
@@ -124,7 +125,22 @@ trend_mon <- function(cname, mon){
   abline(a=upper, b=0, col=3)  
   abline(a=lower, b=0, col=3)
   dev.off()
-  
+  ####date=as.character(thisdata$date)
+  M_data$date <- as.Date(as.character(M_data$date), format="%Y%m%d")
+  p <- ggplot(M_data, aes(x1=sessions, x2=CMA))+#(M_data, aes(x=M_data$date, y=M_data$CMA))+#, linetype=country)) + 
+    geom_line(aes(x=M_data$date, y=M_data$sessions, colour="sessions"), M_data)+
+    geom_line(aes(x=M_data$date, y=M_data$CMA, colour="CMA"), M_data)+
+    geom_hline(yintercept=mean, color="#0099FF")+
+    geom_hline(yintercept=upper, color="#3399FF", linetype="dashed")+ #linetype="dashed"
+    geom_hline(yintercept=lower, color="#3399FF", linetype="dashed")+
+    scale_x_date(labels = date_format("%m%d"))+
+    labs(title = paste(cname)
+         , x = "date"
+         , y = "sessions")+
+    scale_colour_manual("legend", values = c("#CC66FF", "#333333")) 
+    
+    
+  ###
   last_mon <- M_data[(length(M_data$date)-29):length(M_data$date),5]
   U_count <- sum(last_mon > upper)
   L_count <- sum(last_mon < lower)
@@ -153,4 +169,4 @@ C <- as.data.frame(B)
 lan_list<- c("en-au", "", "", "", "cs-cz", "", "fr-fr", "de-de", "", "zh-hk", "", "en-in", "", "", "it-it", "ja-jp", "es-mx", "nl-nl", "", "pl-pl", "pt-pt", "", "", "ko-kr", "es-es", "sv-se", "", "zh-tw", "th-th", "", "en-uk", "en-us")
 C$lan <- lan_list
 as.data.frame(C)
-write.csv(C, 'stat_lan_v3.csv')
+write.csv(C, paste("../", mon, "/stat_lan_v3.csv", sep=""))
